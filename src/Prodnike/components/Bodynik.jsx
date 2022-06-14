0f0ced0f447b40019343f2b7733b7ad25cb0381c
Israel Tiñini <israeltinini2@gmail.com>
2022-06-13 22:59:48 -0400

    parteISRAELfinal

--- src/Prodnike/components/Bodynik.jsx
+++ src/Prodnike/components/Bodynik.jsx
@@ -8,9 +8,10 @@ import imnik4 from "../images/NAIK4.jpg"
 import imnik5 from "../images/NAIK5.jpg"
 import imnik6 from "../images/NAIK6.jpg"
 import imnik7 from "../images/NAIK7.jpg"
-import imnik8 from "../images/NAIK8.jpg"
 import imnik10 from "../images/NAIK10.jpg"
 import imnik9 from "../images/NAIK9.jpg"
+import imnik8 from "../images/NAIK8.jpg"
+
 
 
 const cards = [
